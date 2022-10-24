cb95a8e7b1dac8ba3ddf1a513b447cc22459a0fd
Calvin Bochulak <cbochula@cisco.com>
2022-10-24 10:46:50 -0600

    Add commands.lua

--- lua/grapple/commands.lua
+++ lua/grapple/commands.lua
@@ -3,29 +3,40 @@ local marks = require("grapple.marks")
 
 local M = {}
 
+local function parse_mark_options(fargs)
+    local mark_options = {}
+    for _, arg_string in pairs(fargs) do
+        local delimiter_location = string.find(arg_string, "=")
+        local arg_name = string.sub(arg_string, 1, delimiter_location - 1)
+        local arg_value = string.sub(arg_string, delimiter_location + 1)
+        mark_options[arg_name] = tonumber(arg_value) or arg_value
+    end
+    return mark_options
+end
+
 function M.create_commands()
     vim.api.nvim_create_user_command(
         "GrappleMark",
-        function(opts) marks.mark(opts.args) end,
-        { desc = "Mark a buffer" }
+        function(opts) marks.mark(parse_mark_options(opts.fargs)) end,
+        { desc = "Mark a buffer", nargs = "*" }
     )
 
     vim.api.nvim_create_user_command(
         "GrappleUnmark",
-        function(opts) marks.unmark(opts.args) end,
-        { desc = "Unmark a buffer" }
+        function(opts) marks.unmark(parse_mark_options(opts.fargs)) end,
+        { desc = "Unmark a buffer", nargs = "*" }
     )
 
     vim.api.nvim_create_user_command(
         "GrappleToggle",
-        function(opts) marks.toggle(opts.args) end,
-        { desc = "Toggle a mark" }
+        function(opts) marks.toggle(parse_mark_options(opts.fargs)) end,
+        { desc = "Toggle a mark", nargs = "*" }
     )
 
     vim.api.nvim_create_user_command(
         "GrappleSelect",
-        function(opts) marks.select(opts.args) end,
-        { desc = "Select a mark" }
+        function(opts) marks.select(parse_mark_options(opts.fargs)) end,
+        { desc = "Select a mark", nargs = "*" }
     )
 
     vim.api.nvim_create_user_command(
