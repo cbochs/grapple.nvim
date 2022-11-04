95f70ea71375e699f33a88366f24ce5936122836
Calvin Bochulak <cbochula@cisco.com>
2022-11-04 14:30:40 -0600

    chore(integrations): remove debug statements

--- lua/resession/extensions/grapple.lua
+++ lua/resession/extensions/grapple.lua
@@ -3,14 +3,12 @@ local M = {}
 ---Get the saved data for this extension
 ---@return any
 M.on_save = function()
-    print("Saving tags")
     return require("grapple.tags")._raw_save()
 end
 
 ---Restore the extension state
 ---@param data table The value returned from on_save
 M.on_load = function(data)
-    print("Loading tags")
     require("grapple.tags")._raw_load(data)
 end
 
