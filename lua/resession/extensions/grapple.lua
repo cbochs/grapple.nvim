7fcce14f9cde8b661df0231e7d6b5d807ec6a24c
Calvin Bochulak <cbochula@cisco.com>
2022-11-04 14:09:04 -0600

    feat(integrations): add methods for directly manipulating tag state

--- lua/resession/extensions/grapple.lua
+++ lua/resession/extensions/grapple.lua
@@ -3,13 +3,15 @@ local M = {}
 ---Get the saved data for this extension
 ---@return any
 M.on_save = function()
-    return require("grapple.tags")._tags
+    print("Saving tags")
+    return require("grapple.tags")._raw_save()
 end
 
 ---Restore the extension state
 ---@param data table The value returned from on_save
 M.on_load = function(data)
-    require("grapple.tags")._tags = data
+    print("Loading tags")
+    require("grapple.tags")._raw_load(data)
 end
 
 return M
