51465692dee5ecb45c4d20fd25668d814f63fd89
Calvin Bochulak <cbochula@cisco.com>
2022-11-24 22:29:17 -0700

    refactor: remove slow lualine integration

--- lua/lualine/components/grapple.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    local highlight = require("grapple.highlight")
-    local tag_key = require("grapple").key({ buffer = 0 })
-    if tag_key ~= nil then
-        return "%#" .. highlight.groups.lualine_tag_active .. "#" .. "[" .. tostring(tag_key) .. "]" .. "%*"
-    else
-        return "%#" .. highlight.groups.lualine_tag_inactive .. "#" .. "[U]" .. "%*"
-    end
-end
