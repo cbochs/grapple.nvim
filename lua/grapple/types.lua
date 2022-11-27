0cd6dccfe17cb62957b9f3c44c4346cb846cf5fd
Calvin Bochulak <cbochula@cisco.com>
2022-11-26 22:24:18 -0700

    refactor: remove types.scope

--- lua/grapple/types.lua
+++ lua/grapple/types.lua
@@ -6,13 +6,4 @@ M.direction = {
     forward = "forward",
 }
 
----@enum table<string, Grapple.ScopeKey>
-M.scope = {
-    none = "none",
-    global = "global",
-    static = "static",
-    directory = "directory",
-    lsp = "lsp",
-}
-
 return M
