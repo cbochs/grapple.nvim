f94aeb20737bb192395f3104fbe6f8beec356ef0
Calvin Bochulak <cbochula@cisco.com>
2022-12-06 07:07:29 -0700

    refactor: separate methods for quickfix module

--- lua/grapple/quickfix.lua
+++ lua/grapple/quickfix.lua
@@ -16,15 +16,19 @@ function quickfix.set(quickfix_items)
     vim.fn.setqflist(quickfix_items, "r")
 end
 
-function quickfix.open(quickfix_title)
+function quickfix.title(quickfix_title)
     vim.fn.setqflist({}, "a", { title = quickfix_title })
+end
+
+function quickfix.open()
     vim.api.nvim_cmd({ cmd = "copen" }, {})
 end
 
 function quickfix.send(title, items, quickfixer)
     local quickfix_items = quickfix.from(items, quickfixer)
     quickfix.set(quickfix_items)
-    quickfix.open(title)
+    quickfix.title(title)
+    quickfix.open()
 end
 
 return quickfix
