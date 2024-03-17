0d0fc2b678d7830804f514bd0f57ce5972f0068d
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-17 13:45:12 -0600

    test: add more test cases for TagContent.minimum_column

--- tests/grapple/tag_content_spec.lua
+++ tests/grapple/tag_content_spec.lua
@@ -6,21 +6,35 @@ describe("TagContent", function()
             -- ID only
             { 5, "/000 /some_path" },
             { 5, "/000 " },
+            { 5, "/000           " },
+
+            -- ID + name
+            { 9, "/001 bob /some_path" },
+            { 9, "/001 bob " },
+            { 7, "/001 a /some_path" },
+            { 7, "/001 a " },
+            { 7, "/001 a           " },
 
             -- ID + icon
             { 9, "/001  /some_path" },
-            { 9, "/001  " },
+            { 9, "/002  /some_path" },
+            { 9, "/003  /some_path" },
+            { 9, "/004  " },
+            { 9, "/005            " },
 
             -- ID + icon + name
             { 13, "/001  bob /some_path" },
             { 13, "/001  bob " },
+            { 11, "/001  c /some_path" },
+            { 11, "/001  c " },
+            { 11, "/001  c           " },
         }
 
         for _, test_case in ipairs(test_cases) do
             local expected = test_case[1]
             local line = test_case[2]
 
-            it(string.format("line %s, min_col %d", line, expected), function()
+            it(string.format('expected col %d, line "%s"', expected, line), function()
                 assert.same(expected, TagContent:new(nil, nil, nil, nil):minimum_column(line))
             end)
         end
